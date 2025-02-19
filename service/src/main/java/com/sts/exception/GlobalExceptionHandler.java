67bda3ea581786e14d4a88f829b12e825fc1566c
cuong.nguyenc <cuong.nguyenc@saigontechnology.com>
2025-02-19 17:05:32 +0700

    log error

--- service/src/main/java/com/sts/exception/GlobalExceptionHandler.java
+++ service/src/main/java/com/sts/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.sts.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -13,6 +15,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(Exception ex, WebRequest request) {
+        log.error("Error: ", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
